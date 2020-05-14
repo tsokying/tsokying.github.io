93d848097c97679f6b7a9e29d8bdcdb075af6e01
tsokying <cameronchanty@gmail.com>
2020-05-14 22:27:11 +0800

    Add some JS

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 // coded by @tsokying
 // eslint-disable-next-line no-unused-vars
-const projectName = "FreeCodeCamp_Project5";
+const projectName = "My portfolio";
+
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+// image modal
+var modal = document.getElementById("myModal");
+var i;
+var img = document.getElementsByClassName("image");
+var modalImg = document.getElementById("img01");
+
+ for(i=0;i< img.length;i++)
+   {    
+    img[i].onclick = function(){
+    modal.style.display = "block";
+	modalImg.src = this.src;
+ }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+   modal.style.display = "none";
+}
